0c20fc061fc6972c42e44d10c9c18a1d25158944
A.J. Thomas <ajemeister@gmail.com>
2014-05-16 09:12:37 -0400

    remove warnings

--- SSCHUUAC4/src/com/example/sschuuac4/MainActivity.java
+++ SSCHUUAC4/src/com/example/sschuuac4/MainActivity.java
@@ -1,17 +1,10 @@
 package com.example.sschuuac4;
 
-import android.animation.Animator;
-import android.animation.Animator.AnimatorListener;
+
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
-import android.animation.PropertyValuesHolder;
-import android.animation.ValueAnimator;
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.LinearInterpolator;
-import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 
 public class MainActivity extends Activity {
@@ -21,31 +14,32 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        ObjectAnimator outside = new ObjectAnimator().ofFloat(this.findViewById(R.id.outer), ImageView.ROTATION, 0.0f, 30.0f);
-        outside.setRepeatCount(outside.INFINITE);
+        ObjectAnimator outside = ObjectAnimator.ofFloat(this.findViewById(R.id.outer), ImageView.ROTATION, 0.0f, 30.0f);
+        outside.setRepeatCount(ObjectAnimator.INFINITE);
         outside.setDuration(5000);
         outside.addListener(new ObjectAnimatorRingListener());
         
-        ObjectAnimator middle = new ObjectAnimator().ofFloat(this.findViewById(R.id.middle), ImageView.ROTATION, 0f, -30.0f);
-        middle.setRepeatCount(outside.INFINITE);
+        ObjectAnimator middle = ObjectAnimator.ofFloat(this.findViewById(R.id.middle), ImageView.ROTATION, 0f, -30.0f);
+        middle.setRepeatCount(ObjectAnimator.INFINITE);
         middle.setDuration(5000);
         middle.addListener(new ObjectAnimatorRingListener());
         
-        ObjectAnimator inside = new ObjectAnimator().ofFloat(this.findViewById(R.id.inner), ImageView.ROTATION, 0.0f, 30.0f);
-        inside.setRepeatCount(outside.INFINITE);
+        
+		ObjectAnimator inside = ObjectAnimator.ofFloat(this.findViewById(R.id.inner), ImageView.ROTATION, 0.0f, 30.0f);
+        inside.setRepeatCount(ObjectAnimator.INFINITE);
         inside.setDuration(5000);
         inside.addListener(new ObjectAnimatorRingListener());
         
         
         
-        @SuppressWarnings("static-access")
-		ObjectAnimator fill = new ObjectAnimator().ofFloat(this.findViewById(R.id.fill), "alpha", 1.0f, 0f);
-        fill.setRepeatCount(fill.INFINITE);
+        
+		ObjectAnimator fill = ObjectAnimator.ofFloat(this.findViewById(R.id.fill), "alpha", 1.0f, 0f);
+        fill.setRepeatCount(ObjectAnimator.INFINITE);
         fill.setDuration(5000);
         fill.addListener(new ObjectAnimatorFillListener());
         @SuppressWarnings("static-access")
 		ObjectAnimator smiley = new ObjectAnimator().ofFloat(this.findViewById(R.id.smiley), "alpha", 0.0f, 1.0f);
-        smiley.setRepeatCount(smiley.INFINITE);
+        smiley.setRepeatCount(ObjectAnimator.INFINITE);
         smiley.setDuration(5000);
         smiley.addListener(new ObjectAnimatorFillListener());
         
